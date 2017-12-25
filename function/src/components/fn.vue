0340bd2028705925ca28f1659137641d0d1e389b
SHIZHEMING138 <SHIZHEMING138@pingan.com.cn>
2017-12-25 09:37:33 +0800

    predicate

--- function/src/components/fn.vue
+++ function/src/components/fn.vue
@@ -1,21 +1,12 @@
 <template>
 	<div class="ssss">
 		<div class="name">tobe</div>
-		<div class="left">
-			<h2>动词</h2>
-			<ul>
-				<li><a href="#clone">_.clone</a></li>
-				<li><a href="#randomNumber">_.randomNumber</a></li>
-				<li><a href="#randomAlphabet">_.randomAlphabet</a></li>
-				<li><a href="#randomNumberAlphabet">_.randomNumberAlphabet</a></li>
-				<li><a href="#randomColor">_.randomColor</a></li>
-			</ul>
-		</div>
+		<sidebar></sidebar>
 		<div class="content">
 			<div id="clone">
 				<h4>_.clone(obj)</h4>
 				<div class="describe">
-					<p>复制对象</p>
+					<p>复制对象（浅复制）</p>
 					<h5>参数</h5>
 					<div>obj<em>（Array || Object）</em>：需要复制的对象</div>
 					<h5>返回值</h5>
@@ -103,12 +94,152 @@
 </pre></div>
 				<div style="background-color:hsl(328, 50%, 50%);width:100px;height:100px;display:inline-block;"></div><div style="background-color:hsl(24, 70%, 70%);width:100px;height:100px;display:inline-block;"></div><div style="background-color:hsl(17, 5%, 46%);width:100px;height:100px;display:inline-block;"></div>
 			</div>
+			<div id="isNaN">
+				<h4>_.isNaN(n)</h4>
+				<div class="describe">
+					<p>判断一个值是不是NaN</p>
+					<h5>参数</h5>
+					<div>n<em>（all data type）</em>：需要判断的值</div>
+					<h5>返回值</h5>
+					<div><em>（Boolean）</em>：返回布尔值</div>
+				</div>
+<div class="highlight" style="background: #272822"><pre style="line-height: 125%"><span style="color: #a6e22e">_</span><span style="color: #f8f8f2">.isNaN(</span><span style="color: #ae81ff">1</span><span style="color: #f8f8f2">);</span>
+<span style="color: #75715e">// =&gt; false</span>
+<span style="color: #a6e22e">_</span><span style="color: #f8f8f2">.isNaN(</span><span style="color: #66d9ef">NaN</span><span style="color: #f8f8f2">);</span>
+<span style="color: #75715e">// =&gt; true</span>
+</pre></div>
+			</div>
+			<div id="isNumber">
+				<h4>_.isNumber(n)</h4>
+				<div class="describe">
+					<h5>参数</h5>
+					<div>n<em>（all data type）</em>：需要判断的值</div>
+					<h5>返回值</h5>
+					<div><em>（Boolean）</em>：返回布尔值</div>
+				</div>
+<div class="highlight" style="background: #272822"><pre style="line-height: 125%"><span style="color: #a6e22e">_</span><span style="color: #f8f8f2">.</span><span style="color: #a6e22e">isNumber</span><span style="color: #f8f8f2">(</span><span style="color: #ae81ff">564</span><span style="color: #f8f8f2">);</span>
+<span style="color: #75715e">// =&gt; true</span>
+<span style="color: #a6e22e">_</span><span style="color: #f8f8f2">.</span><span style="color: #a6e22e">isNumber</span><span style="color: #f8f8f2">(</span><span style="color: #e6db74">&#39;66&#39;</span><span style="color: #f8f8f2">);</span>
+<span style="color: #75715e">// =&gt; false</span>
+</pre></div>
+			</div>
+			<div id="isInteger">
+				<h4>_.isInteger(num)</h4>
+				<div class="describe">
+					<p>判断一个数是不是整数</p>
+					<h5>参数</h5>
+					<div>num<em>（Number）</em>：需要判断的数</div>
+					<h5>返回值</h5>
+					<div><em>（Boolean）</em>：返回布尔值</div>
+				</div>
+<div class="highlight" style="background: #272822"><pre style="line-height: 125%"><span style="color: #a6e22e">_</span><span style="color: #f8f8f2">.</span><span style="color: #a6e22e">isInteger</span><span style="color: #f8f8f2">(</span><span style="color: #ae81ff">8</span><span style="color: #f8f8f2">);</span>
+<span style="color: #75715e">// =&gt; true</span>
+<span style="color: #a6e22e">_</span><span style="color: #f8f8f2">.</span><span style="color: #a6e22e">isInteger</span><span style="color: #f8f8f2">(</span><span style="color: #f92672">-</span><span style="color: #ae81ff">8</span><span style="color: #f8f8f2">);</span>
+<span style="color: #75715e">// =&gt; true</span>
+<span style="color: #a6e22e">_</span><span style="color: #f8f8f2">.</span><span style="color: #a6e22e">isInteger</span><span style="color: #f8f8f2">(</span><span style="color: #ae81ff">8.1</span><span style="color: #f8f8f2">);</span>
+<span style="color: #75715e">// =&gt; false</span>
+</pre></div>
+			</div>
+			<div id="isPast">
+				<h4>_.isPast(time)</h4>
+				<div class="describe">
+					<h5>参数</h5>
+					<div>time<em>（Number）</em>：需要时间戳</div>
+					<h5>返回值</h5>
+					<div><em>（Boolean）</em>：返回布尔值</div>
+				</div>
+<div class="highlight" style="background: #272822"><pre style="line-height: 125%"><span style="color: #a6e22e">_</span><span style="color: #f8f8f2">.</span><span style="color: #a6e22e">isPast</span><span style="color: #f8f8f2">((</span><span style="color: #66d9ef">new</span> <span style="color: #f8f8f2">Date(</span><span style="color: #ae81ff">2011</span><span style="color: #f8f8f2">,</span> <span style="color: #ae81ff">11</span><span style="color: #f8f8f2">,</span> <span style="color: #ae81ff">12</span><span style="color: #f8f8f2">)).</span><span style="color: #a6e22e">getTime</span><span style="color: #f8f8f2">());</span>
+<span style="color: #75715e">// =&gt; true</span>
+<span style="color: #a6e22e">_</span><span style="color: #f8f8f2">.</span><span style="color: #a6e22e">isPast</span><span style="color: #f8f8f2">((</span><span style="color: #66d9ef">new</span> <span style="color: #f8f8f2">Date(</span><span style="color: #ae81ff">2991</span><span style="color: #f8f8f2">,</span> <span style="color: #ae81ff">5</span><span style="color: #f8f8f2">,</span> <span style="color: #ae81ff">20</span><span style="color: #f8f8f2">)).</span><span style="color: #a6e22e">getTime</span><span style="color: #f8f8f2">());</span>
+<span style="color: #75715e">// =&gt; false</span>
+</pre></div>
+			</div>
+			<div id="isFuture">
+				<h4>_.isFuture(time)</h4>
+				<div class="describe">
+					<h5>参数</h5>
+					<div>time<em>（Number）</em>：需要时间戳</div>
+					<h5>返回值</h5>
+					<div><em>（Boolean）</em>：返回布尔值</div>
+				</div>
+<div class="highlight" style="background: #272822"><pre style="line-height: 125%"><span style="color: #a6e22e">_</span><span style="color: #f8f8f2">.</span><span style="color: #a6e22e">isFuture</span><span style="color: #f8f8f2">((</span><span style="color: #66d9ef">new</span> <span style="color: #f8f8f2">Date(</span><span style="color: #ae81ff">2011</span><span style="color: #f8f8f2">,</span> <span style="color: #ae81ff">11</span><span style="color: #f8f8f2">,</span> <span style="color: #ae81ff">12</span><span style="color: #f8f8f2">)).</span><span style="color: #a6e22e">getTime</span><span style="color: #f8f8f2">());</span>
+<span style="color: #75715e">// =&gt; false</span>
+<span style="color: #a6e22e">_</span><span style="color: #f8f8f2">.</span><span style="color: #a6e22e">isFuture</span><span style="color: #f8f8f2">((</span><span style="color: #66d9ef">new</span> <span style="color: #f8f8f2">Date(</span><span style="color: #ae81ff">2991</span><span style="color: #f8f8f2">,</span> <span style="color: #ae81ff">5</span><span style="color: #f8f8f2">,</span> <span style="color: #ae81ff">20</span><span style="color: #f8f8f2">)).</span><span style="color: #a6e22e">getTime</span><span style="color: #f8f8f2">());</span>
+<span style="color: #75715e">// =&gt; true</span>
+</pre></div>
+			</div>
+			<div id="isArguments">
+				<h4>_.isArguments(n)</h4>
+				<div class="describe">
+					<h5>参数</h5>
+					<div>n<em>（all data type）</em>：需要判断的值</div>
+					<h5>返回值</h5>
+					<div><em>（Boolean）</em>：返回布尔值</div>
+				</div>
+<div class="highlight" style="background: #272822"><pre style="line-height: 125%"><span style="color: #f8f8f2">(</span><span style="color: #66d9ef">function</span> <span style="color: #f8f8f2">(</span><span style="color: #a6e22e">a</span><span style="color: #f8f8f2">)</span> <span style="color: #f8f8f2">{</span>
+    <span style="color: #a6e22e">_</span><span style="color: #f8f8f2">.</span><span style="color: #a6e22e">isArguments</span><span style="color: #f8f8f2">(</span><span style="color: #a6e22e">arguments</span><span style="color: #f8f8f2">);</span>
+    <span style="color: #75715e">// =&gt; true</span>
+    <span style="color: #a6e22e">_</span><span style="color: #f8f8f2">.</span><span style="color: #a6e22e">isArguments</span><span style="color: #f8f8f2">(</span><span style="color: #a6e22e">a</span><span style="color: #f8f8f2">);</span>
+    <span style="color: #75715e">// =&gt; false</span>
+<span style="color: #f8f8f2">})(</span><span style="color: #ae81ff">1</span><span style="color: #f8f8f2">);</span>
+</pre></div>
+			</div>
+			<div id="isFunction">
+				<h4>_.isFunction(n)</h4>
+				<div class="describe">
+					<h5>参数</h5>
+					<div>n<em>（all data type）</em>：需要判断的值</div>
+					<h5>返回值</h5>
+					<div><em>（Boolean）</em>：返回布尔值</div>
+				</div>
+<div class="highlight" style="background: #272822"><pre style="line-height: 125%"><span style="color: #a6e22e">_</span><span style="color: #f8f8f2">.</span><span style="color: #a6e22e">isFunction</span><span style="color: #f8f8f2">(</span><span style="color: #66d9ef">function</span> <span style="color: #f8f8f2">()</span> <span style="color: #f8f8f2">{});</span>
+<span style="color: #75715e">// =&gt; true</span>
+<span style="color: #a6e22e">_</span><span style="color: #f8f8f2">.</span><span style="color: #a6e22e">isFunction</span><span style="color: #f8f8f2">(</span><span style="color: #e6db74">&#39;abc&#39;</span><span style="color: #f8f8f2">);</span>
+<span style="color: #75715e">// =&gt; false</span>
+</pre></div>
+			</div>
+			<div id="isString">
+				<h4>_.isString(n)</h4>
+				<div class="describe">
+					<h5>参数</h5>
+					<div>n<em>（all data type）</em>：需要判断的值</div>
+					<h5>返回值</h5>
+					<div><em>（Boolean）</em>：返回布尔值</div>
+				</div>
+<div class="highlight" style="background: #272822"><pre style="line-height: 125%"><span style="color: #a6e22e">_</span><span style="color: #f8f8f2">.</span><span style="color: #a6e22e">isString</span><span style="color: #f8f8f2">(</span><span style="color: #e6db74">&#39;abc&#39;</span><span style="color: #f8f8f2">);</span>
+<span style="color: #75715e">// =&gt; true</span>
+<span style="color: #a6e22e">_</span><span style="color: #f8f8f2">.</span><span style="color: #a6e22e">isString</span><span style="color: #f8f8f2">(</span><span style="color: #ae81ff">12</span><span style="color: #f8f8f2">);</span>
+<span style="color: #75715e">// =&gt; false</span>
+</pre></div>
+			</div>
+			<div id="isEqual">
+				<h4>_.isEqual(obj1, obj2)</h4>
+				<div class="describe">
+					<h5>参数</h5>
+					<div>obj1<em>（Object || Array）</em>：需要对比的第一个值</div>
+					<div>obj2<em>（Object || Array）</em>：需要对比的第二个值</div>
+					<h5>返回值</h5>
+					<div><em>（Boolean）</em>：返回布尔值</div>
+				</div>
+<div class="highlight" style="background: #272822"><pre style="line-height: 125%"><span style="color: #a6e22e">_</span><span style="color: #f8f8f2">.</span><span style="color: #a6e22e">isEqual</span><span style="color: #f8f8f2">([</span><span style="color: #ae81ff">1</span><span style="color: #f8f8f2">,</span> <span style="color: #ae81ff">2</span><span style="color: #f8f8f2">,</span> <span style="color: #ae81ff">3</span><span style="color: #f8f8f2">],</span> <span style="color: #f8f8f2">[</span><span style="color: #ae81ff">1</span><span style="color: #f8f8f2">,</span> <span style="color: #ae81ff">2</span><span style="color: #f8f8f2">,</span> <span style="color: #ae81ff">3</span><span style="color: #f8f8f2">]);</span>
+<span style="color: #75715e">// =&gt; true</span>
+<span style="color: #a6e22e">_</span><span style="color: #f8f8f2">.</span><span style="color: #a6e22e">isEqual</span><span style="color: #f8f8f2">([</span><span style="color: #ae81ff">1</span><span style="color: #f8f8f2">,</span> <span style="color: #ae81ff">2</span><span style="color: #f8f8f2">,</span> <span style="color: #ae81ff">3</span><span style="color: #f8f8f2">],</span> <span style="color: #f8f8f2">[</span><span style="color: #ae81ff">1</span><span style="color: #f8f8f2">,</span> <span style="color: #ae81ff">2</span><span style="color: #f8f8f2">,</span> <span style="color: #ae81ff">3</span><span style="color: #f8f8f2">,</span> <span style="color: #ae81ff">4</span><span style="color: #f8f8f2">]);</span>
+<span style="color: #75715e">// =&gt; false</span>
+
+<span style="color: #a6e22e">_</span><span style="color: #f8f8f2">.</span><span style="color: #a6e22e">isEqual</span><span style="color: #f8f8f2">({</span><span style="color: #a6e22e">a</span> <span style="color: #f92672">:</span> <span style="color: #ae81ff">1</span><span style="color: #f8f8f2">,</span> <span style="color: #a6e22e">b</span> <span style="color: #f92672">:</span> <span style="color: #ae81ff">2</span><span style="color: #f8f8f2">}, {</span><span style="color: #a6e22e">a</span> <span style="color: #f92672">:</span> <span style="color: #ae81ff">1</span><span style="color: #f8f8f2">,</span> <span style="color: #a6e22e">b</span> <span style="color: #f92672">:</span> <span style="color: #ae81ff">2</span><span style="color: #f8f8f2">});</span>
+<span style="color: #75715e">// =&gt; true</span>
+<span style="color: #a6e22e">_</span><span style="color: #f8f8f2">.</span><span style="color: #a6e22e">isEqual</span><span style="color: #f8f8f2">({</span><span style="color: #a6e22e">a</span> <span style="color: #f92672">:</span> <span style="color: #ae81ff">1</span><span style="color: #f8f8f2">,</span> <span style="color: #a6e22e">b</span> <span style="color: #f92672">:</span> <span style="color: #ae81ff">2</span><span style="color: #f8f8f2">}, {</span><span style="color: #a6e22e">a</span> <span style="color: #f92672">:</span> <span style="color: #ae81ff">1</span><span style="color: #f8f8f2">,</span> <span style="color: #a6e22e">b</span> <span style="color: #f92672">:</span> <span style="color: #ae81ff">2</span><span style="color: #f8f8f2">,</span> <span style="color: #a6e22e">c</span> <span style="color: #f92672">:</span> <span style="color: #ae81ff">3</span><span style="color: #f8f8f2">});</span>
+<span style="color: #75715e">// =&gt; false</span>
+</pre></div>
+			</div>
 		</div>
 	</div>
 </template>
 
 <script>
-import _ from '@/js/ecma'; 
+import sidebar from './sidebar.vue';
+import _ from '../js/ecma'; 
+
 export default {
 	name: 'fn',
 	data () {
@@ -118,6 +249,9 @@ export default {
 	},
 	mounted () {
 		// this.getName();
+	},
+	components : {
+		sidebar
 	},
 	methods : {
 		getName () {
@@ -135,27 +269,13 @@ export default {
 .name{
 	font-size:30px;
 	position:fixed;
-	background-color:#fff;
 	width:100%;
 	top:0;
 	height:60px;
 	line-height:60px;
 }
-h2{
-	font-weight:400;
-}
-a{
-	color:#000;
-	text-decoration:none;
-}
-a:hover{
-	text-decoration:underline;
-}
 .ssss{
 	font-family:'Microsoft YaHei';
-}
-dt{
-	font-weight:700;
 }
 .describe{
 	margin:1em;
@@ -181,28 +301,8 @@ pre{
 	margin:0;
 	font-family:'Consolas','Courier New','monospace'
 }
-ul>li{
-	list-style:none;
-}
-.left{
-	position:fixed;
-	top:60px;
-	width:300px;
-	overflow:auto;
-	height:92%;
-	padding-left:10px;
-	box-sizing:border-box;
-	margin-top:0;
-	margin-bottom:0;
-}
-li{
-	line-height:1.8;
-}
-a{
-}
 .content{
 	margin-left:310px;
-	margin-top:60px;
 }
 .content >div{
 	border:1px solid #e8e8e8;
---
+++ function/src/components/sidebar.vue
@@ -0,0 +1,84 @@
+<template>
+    <div class="left">
+    	<h2>动词</h2>
+    	<h3>克隆-复制</h3>
+    	<ul>
+    		<li><a href="#clone">_.clone</a></li>
+    	</ul>
+    	<h3>随机</h3>
+    	<ul>
+    		<li><a href="#randomNumber">_.randomNumber</a></li>
+    		<li><a href="#randomAlphabet">_.randomAlphabet</a></li>
+    		<li><a href="#randomNumberAlphabet">_.randomNumberAlphabet</a></li>
+    		<li><a href="#randomColor">_.randomColor</a></li>
+    	</ul>
+    	<h2>谓词</h2>
+    	<ul>
+    		<li><a href="#isNaN">_.isNaN</a></li>
+    		<li><a href="#isNumber">_.isNumber</a></li>
+    		<li><a href="#isInteger">_.isInteger</a></li>
+    		<li><a href="#isPast">_.isPast</a></li>
+    		<li><a href="#isFuture">_.isFuture</a></li>
+    		<li><a href="#isArguments">_.isArguments</a></li>
+    		<li><a href="#isFunction">_.isFunction</a></li>
+    		<li><a href="#isFuture">_.isFuture</a></li>
+    		<li><a href="#isFuture">_.isFuture</a></li>
+    		<li><a href="#isFuture">_.isFuture</a></li>
+    		<li><a href="#isFuture">_.isFuture</a></li>
+    		<li><a href="#isFuture">_.isFuture</a></li>
+    		<li><a href="#isFuture">_.isFuture</a></li>
+    		<li><a href="#isFuture">_.isFuture</a></li>
+    		<li><a href="#isFuture">_.isFuture</a></li>
+    		<li><a href="#isFuture">_.isFuture</a></li>
+    		<li><a href="#isFuture">_.isFuture</a></li>
+    		<li><a href="#isFuture">_.isFuture</a></li>
+    		<li><a href="#isFuture">_.isFuture</a></li>
+    		<li><a href="#isEqual">_.isEqual</a></li>
+    	</ul>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'sidebar',
+  data () {
+    return {
+      msg: 'asdasdasd'
+    }
+  }
+}
+</script>
+
+
+<style>
+h2, h3{
+	font-weight:400;
+}
+h3{
+	font-style:italic;
+}
+a{
+	color:#000;
+	text-decoration:none;
+}
+a:hover{
+	text-decoration:underline;
+}
+ul>li{
+	list-style:none;
+}
+.left{
+	position:fixed;
+	top:60px;
+	width:300px;
+	overflow:auto;
+	height:92%;
+	padding-left:10px;
+	box-sizing:border-box;
+	margin-top:0;
+	margin-bottom:0;
+}
+li{
+	line-height:1.8;
+}
+</style>
